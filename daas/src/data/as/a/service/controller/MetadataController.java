dd4e2101f19fbb2afaaaa43c389bff0fcda9d80a
HiBrant <sadfishliujian@qq.com>
2014-02-26 16:00:53 +0800

    fix

--- daas/src/data/as/a/service/controller/MetadataController.java
+++ daas/src/data/as/a/service/controller/MetadataController.java
@@ -2,12 +2,14 @@ package data.as.a.service.controller;
 
 import net.sf.json.JSONObject;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import data.as.a.service.exception.SystemException;
 import data.as.a.service.exception.UserException;
@@ -16,14 +18,16 @@ import data.as.a.service.metadata.datamodel.DataModelObject;
 import data.as.a.service.metadata.service.ModelDefinitionExecutor;
 
 @Controller
-public class ModelDefineController {
+public class MetadataController {
 
 	@RequestMapping(value = "/__model", method = RequestMethod.POST)
 	@ResponseBody
-	public Object test(@RequestHeader("daas-app-id") String appid,
-			@RequestHeader("daas-api-key") String apiKey,
+	@ResponseStatus(HttpStatus.CREATED)
+	public Object defineModel(
+			@RequestHeader(value = "daas-app-id", required = false) String appid,
+			@RequestHeader(value = "daas-api-key", required = false) String apiKey,
 			@RequestBody String json) throws UserException, SystemException {
-		
+
 		JSON2DataModelConverter converter = new JSON2DataModelConverter(appid);
 		DataModelObject dmo = converter.convert(JSONObject.fromObject(json));
 
